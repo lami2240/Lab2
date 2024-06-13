7e5e78e8c82fffa48e175e2f9ec7a4a92cdac7ab
geonheena <nageonhee04@snu.ac.kr>
2024-06-13 08:38:10 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
-void func(int cnt) {
+void func(int start, int depth) {
+    if (depth == M) {
+        for (int i = 0; i < M; ++i) {
+            cout << vec[i] << ' ';
+        }
+        cout << endl;
+        return;
+    }
 
-
-
-
-
-
-
+    for (int i = start; i <= N; ++i) {
+        vec.push_back(i);
+        func(i + 1, depth + 1);
+        vec.pop_back();
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(1, 0);
+    return 0;
 }
